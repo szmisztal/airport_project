818ac5eedb7c033f964b86d52ed0613494ed8338
szmisztal <sz.misztal@gmail.com>
2023-10-20 12:03:14 +0200

    refactor airplane file, app client == airplane.py

--- airplane.py
+++ airplane.py
@@ -3,8 +3,8 @@ from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, 
 from data_utils import DataUtils
 
 
-class Client:
-    def __init__(self):
+class Airplane:
+    def __init__(self, x, y, z):
         self.HOST = HOST
         self.PORT = PORT
         self.INTERNET_ADDRESS_FAMILY = INTERNET_ADDRESS_FAMILY
@@ -13,6 +13,9 @@ class Client:
         self.encode_format = encode_format
         self.data_utils = DataUtils()
         self.is_running = True
+        self.x = x
+        self.y = y
+        self.z = z
 
     def start(self):
         with s.socket(INTERNET_ADDRESS_FAMILY, SOCKET_TYPE) as client_socket:
