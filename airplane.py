5a56f5d8ff17eb04e605a28d038bf11128535b1e
szmisztal <sz.misztal@gmail.com>
2023-10-21 11:24:17 +0200

    airplane now is ONLY airplane class

--- airplane.py
+++ airplane.py
@@ -1,28 +1,8 @@
-import socket as s
-from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
-from data_utils import DataUtils
-
-
 class Airplane:
     def __init__(self, x, y, z):
-        self.HOST = HOST
-        self.PORT = PORT
-        self.INTERNET_ADDRESS_FAMILY = INTERNET_ADDRESS_FAMILY
-        self.SOCKET_TYPE = SOCKET_TYPE
-        self.BUFFER = BUFFER
-        self.encode_format = encode_format
-        self.data_utils = DataUtils()
-        self.is_running = True
         self.x = x
         self.y = y
         self.z = z
 
-    def start(self):
-        with s.socket(INTERNET_ADDRESS_FAMILY, SOCKET_TYPE) as client_socket:
-            client_socket.connect((HOST, PORT))
-            while self.is_running:
-                pass
 
-if __name__ == "__main__":
-    airplane = Airplane()
-    airplane.start()
+
