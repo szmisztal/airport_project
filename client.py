7c20b3a96915dec6f35009a5545aee36a6216eee
szmisztal <sz.misztal@gmail.com>
2023-10-21 11:23:10 +0200

    make client/airplane classes alone

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket as s
+from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
+from data_utils import DataUtils
+
+
+class Client:
+    def __init__(self, x, y, z):
+        self.HOST = HOST
+        self.PORT = PORT
+        self.INTERNET_ADDRESS_FAMILY = INTERNET_ADDRESS_FAMILY
+        self.SOCKET_TYPE = SOCKET_TYPE
+        self.BUFFER = BUFFER
+        self.encode_format = encode_format
+        self.data_utils = DataUtils()
+        self.is_running = True
+
+    def start(self):
+        with s.socket(INTERNET_ADDRESS_FAMILY, SOCKET_TYPE) as client_socket:
+            client_socket.connect((HOST, PORT))
+            while self.is_running:
+                pass
