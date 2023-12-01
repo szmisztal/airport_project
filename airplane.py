d06a974397818c72605c8444ddc5d9a9b6f1607d
szmisztal <sz.misztal@gmail.com>
2023-12-01 12:12:49 +0100

    refactor all the airplane code

--- airplane.py
+++ airplane.py
@@ -1,52 +1,59 @@
 import datetime
-import time
-import schedule
-from client import Client
+import random
 
 
 class Airplane:
     def __init__(self, x, y, z):
-        self.client = Client()
         self.date_of_appearance = datetime.datetime.now()
         self.x = x
         self.y = y
         self.z = z
-        self.fuel_reserve = 3
-        self.co_ordinates = (x, y, z)
-        self.manager = self.airplane_manager()
+        self.coordinates = (x, y, z)
+        self.quarter = self.establish_airplane_quarter()
 
-    def fuel_consumption_per_hour(self):
-        self.fuel_reserve -= 1
-        if self.fuel_reserve == 0:
-            print("Out of fuel ! Airplane destroyed...")
-            self.client.stop()
+    @classmethod
+    def establish_init_airplane_coordinates(cls):
+        height = random.randint(2000, 5000)
+        random_int = random.randint(-5000, 5000)
+        constant = 5000
+        neg_constant = -5000
+        possible_coordinates = [
+            [random_int, constant, height],
+            [random_int, neg_constant, height],
+            [constant, random_int, height],
+            [neg_constant, random_int, height]
+        ]
+        choose_option = random.choice(possible_coordinates)
+        coordinates_dict = {
+            "x": choose_option[0],
+            "y": choose_option[1],
+            "z": choose_option[2]
+        }
+        return coordinates_dict
 
-    def airplane_axis_movement(self, axis):
-        if axis == self.x or axis == self.y:
-            if axis < 0:
-                if axis == self.x:
-                    self.x += 200
-                elif axis == self.y:
-                    self.y += 200
-            elif axis > 0:
-                if axis == self.x:
-                    self.x -= 200
-                elif axis == self.y:
-                    self.y -= 200
-        elif axis == self.z:
-            self.z -= 20
-        return (self.x, self.y, self.z)
+    def establish_airplane_quarter(self):
+        if self.x in range(-5000, 0) and self.y in range(0, 5001):
+             return "NW"
+        elif self.x in range(0, 5001) and self.y in range(0, 5001):
+            return "NE"
+        elif self.x in range(-5000, 0) and self.y in range(-5000, 0):
+            return "SW"
+        elif self.x in range(0, 5001) and self.y in range(-5000, 0):
+            return "SE"
 
-    def simulate_airplane_movement(self):
-        self.airplane_axis_movement(self.x)
-        self.airplane_axis_movement(self.y)
-        self.airplane_axis_movement(self.z)
+    def fuel_consumption_in_3_hours(self):
+        current_time = datetime.datetime.now()
+        time_difference = current_time - self.date_of_appearance
+        if time_difference >= datetime.timedelta(seconds = 10800):
+                print("Out of fuel ! Airplane crashed...")
 
-    def airplane_manager(self):
-        schedule.every(1).hour.do(self.fuel_consumption_per_hour)
-        schedule.every(1).second.do(self.simulate_airplane_movement)
-        # while True:
-        #     schedule.run_pending()
+    def simulating_airplane_movement(self):
+        pass
+
 
     def __str__(self):
-        return f"{self.x}, {self.y}, {self.z}"
+        return f"Airplane coordinates: {self.x}, {self.y}, {self.z}"
+
+    def __repr__(self):
+        return str(self)
+
