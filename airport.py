a2e6fe8ed5d437c4898f028ed0424fde601ca10a
szmisztal <sz.misztal@gmail.com>
2023-12-02 13:51:56 +0100

    refactor air corrdiors system, and add zero_point coords

--- airport.py
+++ airport.py
@@ -1,13 +1,18 @@
-import math
 from threading import Lock
+from math_patterns import euclidean_formula
 
 
 class Airport:
     def __init__(self):
         self.lock = Lock()
         self.airport_area = AirportArea(-5000, 5000, -5000, 5000, 0, 5000)
-        self.air_corridor_1 = AirCorridor(-2000, 2000, -500, -400, 0, 2000)
-        self.air_corridor_2 = AirCorridor(-2000, 2000, 400, 500, 0, 2000)
+        self.air_corridor_N = AirCorridor(-2000, 2000, 400, 500, 0, 2000)
+        self.air_corridor_S = AirCorridor(-2000, 2000, -500, -400, 0, 2000)
+        self.starting_landing_point_NW = CustomPoint(-2500, 450, 2000)
+        self.starting_landing_point_NE = CustomPoint(2500, 450, 2000)
+        self.starting_landing_point_SW = CustomPoint(-2500, -450, 2000)
+        self.starting_landing_point_SE = CustomPoint(2500, -450, 2000)
+        self.zero_point = CustomPoint(0, 0 ,0)
         self.airport_lanes = 2
         self.max_airplanes_number_in_the_air = 100
         self.airplanes_in_the_air_list = []
@@ -23,7 +28,7 @@ class Airport:
             for i, airplane_1 in enumerate(self.airplanes_in_the_air_list):
                 for j, airplane_2 in enumerate(self.airplanes_in_the_air_list):
                     if i != j:
-                        distance = self.euclidean_formula(airplane_1, airplane_2)
+                        distance = euclidean_formula(airplane_1, airplane_2)
                         if distance <= 10:
                             print(f"Collision between {airplane_1} and {airplane_2}")
                             self.crashed_airplanes.extend([airplane_1, airplane_2])
@@ -31,15 +36,6 @@ class Airport:
                             message = {"Distance": "You`re to close to another airplane. Change your position"}
                             return message
             self.remove_crashed_airplanes_from_the_list()
-
-    @staticmethod
-    def euclidean_formula(airplane_1, airplane_2):
-        distance = math.sqrt((
-            pow(airplane_1.x - airplane_2.x, 2) +
-            pow(airplane_1.y - airplane_2.y, 2) +
-            pow(airplane_1.z - airplane_2.z, 2)
-        ))
-        return distance
 
     def remove_crashed_airplanes_from_the_list(self):
         for airplane in self.crashed_airplanes:
@@ -55,10 +51,17 @@ class AirportArea:
 
 class AirCorridor:
     def __init__(self, x1, x2, y1, y2, z1, z2):
-        self.occupied = False
+        self.lock = Lock()
         self.length = (x1, x2)
         self.width = (y1, y2)
         self.height = (z1, z2)
+
+
+class CustomPoint:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
 
 
