67d319cf5e3f137b634c35bd4550c323c5a6b572
szmisztal <sz.misztal@gmail.com>
2023-12-02 13:51:31 +0100

    my tests

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-import math
+import math_patterns
 from airplane import Airplane
+from airport import Airport
 
-init_coords = Airplane.establish_init_airplane_coordinates()
-airplane = Airplane(**init_coords)
-print(airplane)
-print(airplane.quarter)
+init = Airplane.establish_init_airplane_coordinates()
+airplane = Airplane(**init)
+airport = Airport()
 
+def xyz(airplane, airport):
+    math_patterns.simulating_airplane_movement(airplane, airport.starting_landing_point_NW, airplane.speed, 1)
+    math_patterns.simulating_landing(airport, airplane, "N")
+
+xyz(airplane, airport)
