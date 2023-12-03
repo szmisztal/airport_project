4d10c2c35ef470ec78207b337e59589afa706ac9
szmisztal <sz.misztal@gmail.com>
2023-12-03 15:23:42 +0100

    my tests

--- test.py
+++ test.py
@@ -2,12 +2,17 @@ import math_patterns
 from airplane import Airplane
 from airport import Airport
 
-init = Airplane.establish_init_airplane_coordinates()
-airplane = Airplane(**init)
-airport = Airport()
 
-def xyz(airplane, airport):
-    math_patterns.simulating_airplane_movement(airplane, airport.starting_landing_point_NW, airplane.speed, 1)
-    math_patterns.simulating_landing(airport, airplane, "N")
+# airport = Airport()
+# for i in range(10):
+#     airplane = Airplane(i)
+#     airport.airplanes_in_the_air_list.append(airplane)
+#
+# while True:
+#     for airplane in airport.airplanes_in_the_air_list:
+#         math_patterns.simulating_airplane_movement(airplane, airport.starting_landing_point_NW, airplane.speed, 1)
+#         airport.check_distance_between_airplanes()
+#         math_patterns.simulating_landing(airport, airplane)
+#         if airport.airplanes_in_the_air_list == 0:
+#             break
 
-xyz(airplane, airport)
