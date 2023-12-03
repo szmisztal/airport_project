d524f5f0ad2c0c7ddc772e5742090c3569b78d17
szmisztal <sz.misztal@gmail.com>
2023-12-03 15:24:07 +0100

    change args in function to be more flexible

--- math_patterns.py
+++ math_patterns.py
@@ -9,29 +9,30 @@ def euclidean_formula(object_1, object_2):
     ))
     return distance
 
-def simulating_airplane_movement(airplane, target, speed, time_interval, comment = None):
-    current_position = airplane
-    while True:
+def simulating_airplane_movement(airplane, target, speed, time_interval):
+    movement_to_target = True
+    while movement_to_target:
         distance = euclidean_formula(airplane, target)
         if distance < speed * time_interval:
-            if comment:
-                print(comment)
-            break
+            movement_to_target = False
         else:
-            delta_x = target.x - current_position.x
-            delta_y = target.y - current_position.y
-            delta_z = target.z - current_position.z
+            delta_x = target.x - airplane.x
+            delta_y = target.y - airplane.y
+            delta_z = target.z - airplane.z
             angle_xy = math.atan2(delta_y, delta_x)
             angle_xz = math.atan2(delta_z, math.sqrt(pow(delta_x, 2) + pow(delta_y, 2)))
             v_x = speed * math.cos(angle_xy) * math.cos(angle_xz)
             v_y = speed * math.sin(angle_xy) * math.cos(angle_xz)
             v_z = speed * math.sin(angle_xz)
-            current_position.x += v_x * time_interval
-            current_position.y += v_y * time_interval
-            current_position.z += v_z * time_interval
-            print(f"X: {current_position.x:.2f}, Y: {current_position.y:.2f}, Z: {current_position.z:.2f}")
+            airplane.x += v_x * time_interval
+            airplane.y += v_y * time_interval
+            airplane.z += v_z * time_interval
 
 def simulating_landing(airport, airplane):
-    simulating_airplane_movement(airplane, airport.zero_point, 200, 1)
+    simulating_airplane_movement(airplane, airport.zero_point, airplane.speed, 1)
+    if 0 <= airplane.z <= 50:
+        return True
+    elif airplane.z < 0:
+        return False
 
 
