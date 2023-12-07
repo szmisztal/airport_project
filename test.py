a614b67e0a8310d4d0fc5291db8493a557611aef
szmisztal <sz.misztal@gmail.com>
2023-12-07 11:35:46 +0100

    my tests

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-import math_patterns
-from airplane import Airplane
+from itertools import product
 from airport import Airport
-
-
-# airport = Airport()
-# for i in range(10):
-#     airplane = Airplane(i)
-#     airport.airplanes_in_the_air_list.append(airplane)
-#
-# while True:
-#     for airplane in airport.airplanes_in_the_air_list:
-#         math_patterns.simulating_airplane_movement(airplane, airport.starting_landing_point_NW, airplane.speed, 1)
-#         airport.check_distance_between_airplanes()
-#         math_patterns.simulating_landing(airport, airplane)
-#         if airport.airplanes_in_the_air_list == 0:
-#             break
+from airplane import Airplane
 
