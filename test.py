0c1e31fe43bc84929d4725f8fc0edcf512646448
szmisztal <sz.misztal@gmail.com>
2023-12-13 16:51:29 +0100

    my tests

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from itertools import product
 from airport import Airport
 from airplane import Airplane
+from connection_pool import ConnectionPool
 
+conn = ConnectionPool(10, 100)
+print(conn.connections_list)
