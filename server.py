4ad4ad4611f6a2d131d01523fe707065c600199d
szmisztal <sz.misztal@gmail.com>
2023-12-13 14:49:45 +0100

    change server ver

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import socket as s
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
-from airport import Airport
-
+from connection_pool import ConnectionPool
 
 class Server:
     def __init__(self):
@@ -14,17 +13,20 @@ class Server:
         self.data_utils = DataUtils()
         self.encode_format = encode_format
         self.is_running = True
-        self.version = "0.0.5"
+        self.version = "0.0.7"
+        self.conn_pool = ConnectionPool(10, 100)
 
     def start(self):
         with s.socket(self.INTERNET_ADDRESS_FAMILY, self.SOCKET_TYPE) as server_socket:
             server_socket.bind((self.HOST, self.PORT))
             server_socket.listen()
             client_socket, address = server_socket.accept()
-            airport = Airport()
             with client_socket:
                 while self.is_running:
                     pass
                     # co_ordinates = airport.establish_init_airplane_co_ordinates()
                     # airplane = Airplane(**co_ordinates)
                     # airport.airplanes_in_air_list.append(airplane)
+
+server = Server()
+server.start()
