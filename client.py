f224c0da0014c9913fd2e8b00c76322be5d9f343
szmisztal <sz.misztal@gmail.com>
2023-12-14 11:45:23 +0100

    add communication services between client and server

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket as s
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
+from airplane import Airplane
+from communication_utils import ClientProtocols
 
 
 class Client:
@@ -13,12 +15,30 @@ class Client:
         self.encode_format = encode_format
         self.data_utils = DataUtils()
         self.is_running = True
+        self.communication_utils = ClientProtocols()
+
+    def read_server_response(self, dict_data):
+        deserialized_data = self.data_utils.deserialize_json(dict_data)
+        if isinstance(deserialized_data, dict):
+            for key, value in deserialized_data.items():
+                print(f">>> {key}: {value}")
 
     def start(self):
         with s.socket(INTERNET_ADDRESS_FAMILY, SOCKET_TYPE) as client_socket:
             client_socket.connect((HOST, PORT))
+            server_response = client_socket.recv(self.BUFFER)
+            self.read_server_response(server_response)
+            initial_coordinates = Airplane.establish_init_airplane_coordinates()
+            client_request = self.communication_utils.initial_coordinates(initial_coordinates)
+            client_socket.sendall(self.data_utils.serialize_to_json(client_request))
             while self.is_running:
-                pass
+                server_response = client_socket.recv(self.BUFFER)
 
     def stop(self):
         self.is_running = False
+
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.start()
--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket as s
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
 from connection_pool import ConnectionPool
+from communication_utils import ServerProtocols
+from airport import Airport
+
 
 class Server:
     def __init__(self):
@@ -13,20 +16,30 @@ class Server:
         self.data_utils = DataUtils()
         self.encode_format = encode_format
         self.is_running = True
-        self.version = "0.0.7"
+        self.version = "0.0.8"
+        self.communication_utils = ServerProtocols()
         self.connection_pool = ConnectionPool(10, 100)
+        self.airport = Airport()
 
     def start(self):
         with s.socket(self.INTERNET_ADDRESS_FAMILY, self.SOCKET_TYPE) as server_socket:
+            print("Server`s up")
             server_socket.bind((self.HOST, self.PORT))
             server_socket.listen()
             client_socket, address = server_socket.accept()
             with client_socket:
+                connection = self.connection_pool.get_connection()
+                welcome_message = self.data_utils.serialize_to_json(self.communication_utils.welcome_protocol())
+                client_socket.sendall(welcome_message)
+                initial_coordinates_json = client_socket.recv(self.BUFFER)
+                initial_coordinates = self.data_utils.deserialize_json(initial_coordinates_json)
+                self.airport.create_airplane_object_and_append_it_to_list(initial_coordinates)
                 while self.is_running:
-                    pass
-                    # co_ordinates = airport.establish_init_airplane_co_ordinates()
-                    # airplane = Airplane(**co_ordinates)
-                    # airport.airplanes_in_air_list.append(airplane)
+                    self.airport.airport_manager()
 
-server = Server()
-server.start()
+
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
