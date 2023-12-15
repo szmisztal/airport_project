f07fe09316089acb82d59fa58258eb5ebf3290ae
szmisztal <sz.misztal@gmail.com>
2023-12-15 11:57:16 +0100

    add new Server/client protocols.

--- communication_utils.py
+++ communication_utils.py
@@ -9,9 +9,15 @@ class CommunicationUtils:
 
 class ServerProtocols(CommunicationUtils):
     def welcome_protocol(self):
-        self.protocol_template(message = "Welcome", body = "Submit your coordinates")
+        return self.protocol_template(message = "Welcome", body = "Submit your coordinates")
+
+    def airplane_crashed(self):
+        return self.protocol_template(message = "Crash !", body = "Airplane`s destroyed...")
+
+    def successfully_landing(self):
+        return self.protocol_template(message = "Success !", body = "You`re successfully landing")
 
 
 class ClientProtocols(CommunicationUtils):
     def initial_coordinates(self, coordinates):
-        self.protocol_template(message = "Coordinates", body = coordinates)
+        return self.protocol_template(message = "Coordinates", body = coordinates)
