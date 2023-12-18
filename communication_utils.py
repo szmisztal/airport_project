aeeb1c29648ab6e7a4504fe9e0b4234cf7bb0338
szmisztal <sz.misztal@gmail.com>
2023-12-18 13:38:54 +0100

    cosmetic changes in messeges between server and client

--- communication_utils.py
+++ communication_utils.py
@@ -9,13 +9,19 @@ class CommunicationUtils:
 
 class ServerProtocols(CommunicationUtils):
     def welcome_protocol(self):
-        return self.protocol_template(message = "Welcome", body = "Submit your coordinates")
+        return self.protocol_template(message = "Welcome !", body = "Submit your coordinates")
 
     def airplane_crashed(self):
-        return self.protocol_template(message = "Crash !", body = "Airplane crashed...")
+        return self.protocol_template(message = "Crash !", body = "Airplane crashed..")
 
     def successfully_landing(self):
-        return self.protocol_template(message = "Success !", body = "You`re successfully landed.")
+        return self.protocol_template(message = "Success !", body = "You`re successfully landed")
+
+    def server_shut_down(self):
+        return self.protocol_template(message = "Server status:", body = "Shutting down..")
+
+    def connections_limit(self):
+        return self.protocol_template(message = "Airport`s full:", body = "You have to fly away..")
 
 
 class ClientProtocols(CommunicationUtils):
