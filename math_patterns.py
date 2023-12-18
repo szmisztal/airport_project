932d3cd05cc30e40f28d4f4396d11fd395da124f
szmisztal <sz.misztal@gmail.com>
2023-12-18 13:38:30 +0100

    temporary comment while loop in def simulate_movement

--- math_patterns.py
+++ math_patterns.py
@@ -7,15 +7,15 @@ def euclidean_formula(object_1, object_2):
         pow(object_1.y - object_2.y, 2) +
         pow(object_1.z - object_2.z, 2)
     ))
-    return distance
+    return round(distance)
 
 def simulating_airplane_movement(airplane, target, time_interval = 1):
-    movement_to_target = True
-    while movement_to_target:
-        distance = euclidean_formula(airplane, target)
-        if distance < airplane.speed * time_interval:
-            movement_to_target = False
-        else:
+    # movement_to_target = True
+    # while movement_to_target:
+    #     distance = euclidean_formula(airplane, target)
+    #     if distance < airplane.speed * time_interval:
+    #         movement_to_target = False
+    #     else:
             delta_x = target.x - airplane.x
             delta_y = target.y - airplane.y
             delta_z = target.z - airplane.z
@@ -24,8 +24,8 @@ def simulating_airplane_movement(airplane, target, time_interval = 1):
             v_x = airplane.speed * math.cos(angle_xy) * math.cos(angle_xz)
             v_y = airplane.speed * math.sin(angle_xy) * math.cos(angle_xz)
             v_z = airplane.speed * math.sin(angle_xz)
-            airplane.x += v_x * time_interval
-            airplane.y += v_y * time_interval
-            airplane.z += v_z * time_interval
+            airplane.x += round(v_x * time_interval)
+            airplane.y += round(v_y * time_interval)
+            airplane.z += round(v_z * time_interval)
             return airplane.x, airplane.y, airplane.z
 
