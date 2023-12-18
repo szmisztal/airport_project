e8e8577e9c8a6450b80ef1f637adcf499c954a3f
szmisztal <sz.misztal@gmail.com>
2023-12-18 13:35:44 +0100

    add dedicated conn for server, add methods to work with db

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+import datetime
 import socket as s
+import time
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
-from connection_pool import ConnectionPool
+from connection_pool import ConnectionPool, Connection
 from communication_utils import ServerProtocols
 from airport import Airport
 
@@ -16,8 +18,10 @@ class Server:
         self.data_utils = DataUtils()
         self.encode_format = encode_format
         self.is_running = True
-        self.version = "0.0.8"
+        self.start_date = datetime.datetime.now()
+        self.version = "0.1.1"
         self.communication_utils = ServerProtocols()
+        self.server_connection = Connection()
         self.connection_pool = ConnectionPool(10, 100)
         self.airport = Airport()
 
@@ -30,38 +34,48 @@ class Server:
             if airplane in self.airport.crashed_airplanes:
                 crash_message = self.communication_utils.airplane_crashed()
                 self.send_message_to_client(client_socket, crash_message)
+                self.data_utils.update_status(connection, "Crashed", airplane)
             elif airplane in self.airport.airplanes_with_successfully_landing:
                 landing_message = self.communication_utils.successfully_landing()
                 self.send_message_to_client(client_socket, landing_message)
+                self.data_utils.update_status(connection, "Landed", airplane)
             self.connection_pool.release_connection(connection)
 
     def start(self):
         with s.socket(self.INTERNET_ADDRESS_FAMILY, self.SOCKET_TYPE) as server_socket:
-            print("Server`s up")
+            self.data_utils.create_connections_table(self.server_connection)
+            print("Server`s up".upper())
             server_socket.bind((self.HOST, self.PORT))
             server_socket.listen()
             client_socket, address = server_socket.accept()
             with client_socket:
                 connection = self.connection_pool.get_connection()
-                welcome_message = self.communication_utils.welcome_protocol()
-                self.send_message_to_client(client_socket, welcome_message)
-                initial_coordinates_json = client_socket.recv(self.BUFFER)
-                initial_coordinates = self.data_utils.deserialize_json(initial_coordinates_json)
-                print(initial_coordinates)
-                airplane = self.airport.create_airplane_object_and_append_it_to_list(initial_coordinates["body"])
-                while self.is_running:
-                    print(airplane.coordinates)
-                    break
-                    # for a in self.airport.airplanes_in_the_air_list:
-                    #     print(a.coordinates)
-                    #     a.x += 100
-                    #     a.y += 100
-                    #     a.z -= 10
-                    #     if a.x == 4000 or a.y == 4000 or a.z == 2000:
-                    #         break
+                if connection == False:
+                    self.communication_utils.connections_limit()
+                    client_socket.close()
+                    return
+                else:
+                    welcome_message = self.communication_utils.welcome_protocol()
+                    self.send_message_to_client(client_socket, welcome_message)
+                    initial_coordinates_json = client_socket.recv(self.BUFFER)
+                    initial_coordinates = self.data_utils.deserialize_json(initial_coordinates_json)
+                    airplane = self.airport.create_airplane_object_and_append_it_to_list(initial_coordinates["body"])
+                    self.data_utils.add_new_connection_to_db(connection, str(airplane))
+                    while self.is_running:
+                        self.airport.airport_manager()
+                        print(airplane.x, airplane.y, airplane.z)
+                        self.close_client_socket_when_airplane_crashed_or_successfully_landed(airplane, client_socket, connection)
+                        time.sleep(1)
+                        self.stop(server_socket)
 
-                    # self.airport.airport_manager()
-                    # self.close_client_socket_when_airplane_crashed_or_successfully_landed(airplane, client_socket, connection)
+    def stop(self, server_socket):
+        current_time = datetime.datetime.now()
+        time_difference = current_time - self.start_date
+        if time_difference >= datetime.timedelta(seconds = 3600):
+            print("Server`s out...".upper())
+            self.is_running = False
+            server_socket.close()
+
 
 
 if __name__ == "__main__":
