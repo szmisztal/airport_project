045e1e2ec80e9b27f01b217431a435ae1ea809d7
szmisztal <sz.misztal@gmail.com>
2023-12-19 13:06:36 +0100

    my tests

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ import time
 import math_patterns
 from airport import Airport
 from airplane import Airplane
-# from connection_pool import ConnectionPool
-from math_patterns import simulating_airplane_movement, euclidean_formula
+from connection_pool import ConnectionPool
+from math_patterns import movement_formula, euclidean_formula
 # conn = ConnectionPool(10, 100)
 # print(conn.connections_list)
 
@@ -12,14 +12,16 @@ from math_patterns import simulating_airplane_movement, euclidean_formula
 a = Airplane.establish_init_airplane_coordinates()
 b = Airplane.establish_init_airplane_coordinates()
 airport = Airport()
-airport.create_airplane_object_and_append_it_to_list(a)
-airport.create_airplane_object_and_append_it_to_list(b)
+airport.create_airplane_object_and_append_it_to_list(a, 0)
+airport.create_airplane_object_and_append_it_to_list(b, 1)
 airplane_1 = airport.airplanes_in_the_air_list[0]
 airplane_2 = airport.airplanes_in_the_air_list[1]
 movement = True
 while movement:
     airport.airport_manager()
-    print(airplane_1.x, airplane_1.y, airplane_1.z)
-    print(airplane_2.x, airplane_2.y, airplane_2.z)
-    time.sleep(1)
+    # print(airplane_1.x, airplane_1.y, airplane_1.z)
+    print(airplane_1.move_to_initial_landing_point, airplane_1.move_to_waiting_sector, airplane_1.move_to_runaway)
+    # time.sleep(1)
+
+
 
