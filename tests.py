93f56f7f90c70a37b6b345e58b6edf2a6c5fc27d
szmisztal <sz.misztal@gmail.com>
2023-12-21 18:08:42 +0100

    my tests

--- tests.py
+++ tests.py
@@ -9,19 +9,6 @@ from math_patterns import movement_formula, euclidean_formula
 # print(conn.connections_list)
 
 
-a = Airplane.establish_init_airplane_coordinates()
-b = Airplane.establish_init_airplane_coordinates()
-airport = Airport()
-airport.create_airplane_object_and_append_it_to_list(a, 0)
-airport.create_airplane_object_and_append_it_to_list(b, 1)
-airplane_1 = airport.airplanes_in_the_air_list[0]
-airplane_2 = airport.airplanes_in_the_air_list[1]
-movement = True
-while movement:
-    airport.airport_manager()
-    # print(airplane_1.x, airplane_1.y, airplane_1.z)
-    print(airplane_1.move_to_initial_landing_point, airplane_1.move_to_waiting_sector, airplane_1.move_to_runaway)
-    # time.sleep(1)
 
 
 
