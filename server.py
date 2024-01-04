cbddab7163fac0e3dcbf146383a99686f4ce9877
szmisztal <sz.misztal@gmail.com>
2024-01-04 12:18:25 +0100

    change connection_pool, now it works with db, not clients handler

--- server.py
+++ server.py
@@ -7,80 +7,7 @@ from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, 
 from data_utils import DataUtils
 from communication_utils import ServerProtocols
 from airport import Airport
-
-
-class ConnectionPool:
-    def __init__(self, server, min_numbers_of_connections, max_number_of_connections):
-        self.server = server
-        self.connections_list = []
-        self.connections_in_use_list = []
-        self.lock = Lock()
-        self.min_number_of_connections = min_numbers_of_connections
-        self.max_number_of_connections = max_number_of_connections
-        self.create_start_connections()
-        self.connections_manager()
-
-    def create_start_connections(self):
-        for _ in range(self.min_number_of_connections):
-            connection = ClientHandler(None, None)
-            self.connections_list.append(connection)
-
-    def get_connection(self, client_socket, address):
-        self.lock.acquire()
-        try:
-            for connection in self.connections_list:
-                if connection.in_use == False:
-                    connection.in_use = True
-                    connection.client_socket = client_socket
-                    connection.address = address
-                    self.connections_in_use_list.append(connection)
-                    self.connections_list.remove(connection)
-                    return connection
-            if len(self.connections_list) < self.max_number_of_connections \
-                    and len(self.connections_list) + len(self.connections_in_use_list) < self.max_number_of_connections:
-                new_connection = ClientHandler(client_socket, address)
-                new_connection.in_use = True
-                self.connections_in_use_list.append(new_connection)
-                return new_connection
-            elif len(self.connections_list) + len(self.connections_in_use_list) >= self.max_number_of_connections:
-                return False
-        finally:
-            self.lock.release()
-
-    def release_connection(self, connection):
-        self.lock.acquire()
-        try:
-            if connection in self.connections_in_use_list:
-                connection.in_use = False
-                connection.client_socket = None
-                connection.address = None
-                self.connections_list.append(connection)
-                self.connections_in_use_list.remove(connection)
-        finally:
-            self.lock.release()
-
-    def destroy_unused_connections(self):
-        self.lock.acquire()
-        try:
-            for connection in self.connections_list:
-                if len(self.connections_list) > 11:
-                    self.connections_list.remove(connection)
-                    if len(self.connections_list) == 10:
-                        break
-        finally:
-            self.lock.release()
-
-    def keep_connections_at_the_starting_level(self):
-        if len(self.connections_list) < self.min_number_of_connections:
-            for _ in self.connections_list:
-                connection = ClientHandler(None, None)
-                self.connections_list.append(connection)
-                if len(self.connections_list) == self.min_number_of_connections:
-                    break
-
-    def connections_manager(self):
-        schedule.every(1).minute.do(self.keep_connections_at_the_starting_level)
-        schedule.every(1).minute.do(self.destroy_unused_connections)
+from connection_pool import ConnectionPool
 
 
 class ClientHandler(threading.Thread):
@@ -104,6 +31,7 @@ class ClientHandler(threading.Thread):
         initial_coordinates = self.data_utils.deserialize_json(initial_coordinates_json)
         print(initial_coordinates)
 
+
 class Server:
     def __init__(self):
         self.HOST = HOST
@@ -116,7 +44,7 @@ class Server:
         self.is_running = True
         self.start_date = datetime.datetime.now()
         self.version = "0.2.1"
-        self.connection_pool = ConnectionPool(self, 10, 100)
+        self.connection_pool = ConnectionPool(10, 100)
         self.airport = Airport()
 
     def close_client_socket_when_airplane_crashed_or_successfully_landed(self, airplane, client_socket):
