cb270dc0f5176086039c738a29fdf27ddc3cde12
szmisztal <sz.misztal@gmail.com>
2024-01-17 12:10:38 +0100

    add direct_airplane_protocol method to ServerProtocols

--- communication_utils.py
+++ communication_utils.py
@@ -15,9 +15,13 @@ class ServerProtocols(CommunicationUtils):
     def points_for_airplane_protocol(self, points):
         return self.protocol_template(message = "Your points: ", body = points)
 
+    def direct_airplane_protocol(self, point):
+        return self.protocol_template(message = "Fly to: ", body = point)
+
+
 class ClientProtocols(CommunicationUtils):
-    def send_coordinates_protocol(self, coordinates):
+    def coordinates_protocol(self, coordinates):
         return self.protocol_template(message = "Our coordinates: ", body = coordinates)
 
-    def send_airplane_object_protocol(self, object):
+    def airplane_object_protocol(self, object):
         return self.protocol_template(message = "Our data: ", body = object)
