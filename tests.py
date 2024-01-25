76abfc54fc3db948800634e724670cf06b721aee
szmisztal <sz.misztal@gmail.com>
2024-01-25 12:10:46 +0100

    my tests

--- tests.py
+++ tests.py
@@ -15,24 +15,24 @@ import time
 # ax.set_zlim([0, 5000])
 #
 # ax.scatter(*airport_coords, color='red', label='Airport')
-
+#
 # airplanes = {
 #     'airplane_1': [1000, 1000, 5000],
 #     'airplane_2': [-2000, 1500, 500],
 #     'airplane_3': [3000, -3000, 2000]
 #     }
-
-# airplanes_test = {}
-# airplane_obj_1 = Airplane({'x': 5000, 'y': 3000, 'z': 200})
-# airplane_obj_1.id = 1
-# airplane = {f'Airplane_{airplane_obj_1.id}': [airplane_obj_1.x, airplane_obj_1.y, airplane_obj_1.z]}
-# airplanes_test.update(airplane)
 #
-# airplane_obj_2 = Airplane({'x': -5000, 'y': -3000, 'z': 200})
-# airplane_obj_2.id = 2
-# airplane_2 = {f'Airplane_{airplane_obj_2.id}': [airplane_obj_2.x, airplane_obj_2.y, airplane_obj_2.z]}
-# airplanes_test.update(airplane_2)
+airplanes_test = {}
+airplane_obj_1 = Airplane({'x': 5000, 'y': 3000, 'z': 200})
+airplane_obj_1.id = 1
+airplane = {f'Airplane_{airplane_obj_1.id}': [airplane_obj_1.x, airplane_obj_1.y, airplane_obj_1.z]}
+airplanes_test.update(airplane)
 
+airplane_obj_2 = Airplane({'x': -5000, 'y': -3000, 'z': 200})
+airplane_obj_2.id = 2
+airplane_2 = {f'Airplane_{airplane_obj_2.id}': [airplane_obj_2.x, airplane_obj_2.y, airplane_obj_2.z]}
+airplanes_test.update(airplane_2)
+#
 # for key, value in airplanes_test.items():
 #     ax.scatter(value[0], value[1], value[2], marker = 'o', label = key)
 #
@@ -62,13 +62,6 @@ import time
 #     plt.draw()
 #
 # plt.show()
-def return_point_coordinates(target):
-    match target:
-        case "initial landing point":
-            print(1)
-        case "waiting point":
-            print(2)
-        case "runaway":
-            print(3)
 
-return_point_coordinates("waiting point")
+a = Airport()
+a.radar.config_a_graph(airplanes_test)
