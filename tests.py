93661984a6d5632a9836a41d4b22b88eb4ef0aa0
szmisztal <sz.misztal@gmail.com>
2024-01-27 11:09:25 +0100

    my tests

--- tests.py
+++ tests.py
@@ -1,67 +1,65 @@
-import matplotlib.pyplot as plt
-from airplane import Airplane
 from math_patterns import movement_formula
 from airport import Airport
 import time
+from airplane import Airplane
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-# airport_coords = (0, 0, 0)
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# ax.set_xlim([-5000, 5000])
-# ax.set_ylim([-5000, 5000])
-# ax.set_zlim([0, 5000])
-#
-# ax.scatter(*airport_coords, color='red', label='Airport')
-#
-# airplanes = {
-#     'airplane_1': [1000, 1000, 5000],
-#     'airplane_2': [-2000, 1500, 500],
-#     'airplane_3': [3000, -3000, 2000]
-#     }
-#
-airplanes_test = {}
-airplane_obj_1 = Airplane({'x': 5000, 'y': 3000, 'z': 200})
-airplane_obj_1.id = 1
-airplane = {f'Airplane_{airplane_obj_1.id}': [airplane_obj_1.x, airplane_obj_1.y, airplane_obj_1.z]}
-airplanes_test.update(airplane)
 
-airplane_obj_2 = Airplane({'x': -5000, 'y': -3000, 'z': 200})
-airplane_obj_2.id = 2
-airplane_2 = {f'Airplane_{airplane_obj_2.id}': [airplane_obj_2.x, airplane_obj_2.y, airplane_obj_2.z]}
-airplanes_test.update(airplane_2)
-#
-# for key, value in airplanes_test.items():
-#     ax.scatter(value[0], value[1], value[2], marker = 'o', label = key)
-#
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-#
-# ax.legend()
-#
-# for i in range(10):
-#     for key, value in airplanes_test.items():
-#         value[0] += 500
-#         value[1] += 500
-#         value[2] += 100
-#     plt.pause(1)
-#     ax.clear()
-#     ax.scatter(*airport_coords, color='red', label='Airport')
-#     for key, value in airplanes_test.items():
-#         ax.scatter(value[0], value[1], value[2], marker='o', label=key)
-#     ax.set_xlim([-5000, 5000])
-#     ax.set_ylim([-5000, 5000])
-#     ax.set_zlim([0, 5000])
-#     ax.set_xlabel('X')
-#     ax.set_ylabel('Y')
-#     ax.set_zlabel('Z')
-#     ax.legend()
-#     plt.draw()
-#
-# plt.show()
+init_coords_1 = Airplane.establish_init_airplane_coordinates()
+init_coords_2 = Airplane.establish_init_airplane_coordinates()
+airplane_obj_1 = Airplane(init_coords_1)
+airplane_obj_2 = Airplane(init_coords_2)
 
-a = Airport()
-a.radar.config_a_graph(airplanes_test)
+a_list = []
+a_list.append(airplane_obj_1)
+a_list.append(airplane_obj_2)
+
+
+
+
+# airport_scatter = ax.scatter(*airport_coords, color='red', label='Airport')
+# airplane_scatters = []  # Lista do przechowywania obiektów scatter dla samolotów
+#
+# for a in a_list:
+#     airplane_scatter = ax.scatter(a.x, a.y, a.z, marker='o', label="Airplane")
+#     airplane_scatters.append(airplane_scatter)
+fig = plt.figure()
+
+
+def asd():
+
+    # global a_list
+    ax = fig.add_subplot(111, projection='3d')
+    # Usunięcie wszystkich punktów z wykresu
+    ax.clear()
+    airport_coords = (0, 0, 0)
+    ax.set_xlim([-5000, 5000])
+    ax.set_ylim([-5000, 5000])
+    ax.set_zlim([0, 5000])
+
+    # Dodanie lotniska
+    ax.scatter(*airport_coords, color='red', label='Airport')
+
+    # Dodanie nowych punktów dla każdego samolotu
+    for a in a_list:
+        ax.scatter(a.x, a.y, a.z, marker='o', label="Airplane")
+
+    ax.legend()
+    plt.draw()
+
+
+i = 0
+while i < 10:
+    asd()
+    for airplane in a_list:
+        movement_formula(airplane, 0, 0, 0)
+    if i == 5:
+        init_coords_3 = Airplane.establish_init_airplane_coordinates()
+        airplane_obj_3 = Airplane(init_coords_3)
+        a_list.append(airplane_obj_3)
+    plt.pause(1)
+    i += 1
+
+
