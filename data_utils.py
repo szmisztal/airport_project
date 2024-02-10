de852f61bc55f021fc0b56769513e91e8ab8bee7
szmisztal <sz.misztal@gmail.com>
2024-02-10 11:31:01 +0100

    add doc-strings to all methods in data_utils.py file

--- data_utils.py
+++ data_utils.py
@@ -4,35 +4,83 @@ from variables import encode_format
 
 
 class SerializeUtils:
+    """
+    Utility class for serializing and deserializing data to and from JSON format.
+    """
 
     def serialize_to_json(self, dict_data):
+        """
+        Serialize a dictionary to JSON format.
+
+        Parameters:
+        - dict_data (dict): The dictionary to be serialized.
+
+        Returns:
+        - bytes: The JSON-encoded data.
+        """
         return json.dumps(dict_data).encode(encode_format)
 
     def deserialize_json(self, dict_data):
+        """
+        Deserialize JSON data to a dictionary.
+
+        Parameters:
+        - dict_data (bytes): The JSON-encoded data.
+
+        Returns:
+        - dict: The deserialized dictionary.
+        """
         return json.loads(dict_data)
 
 
 class DatabaseUtils:
+    """
+    Utility class for executing SQL queries and managing database tables.
+    """
 
     def execute_sql_query(self, connection, query, *args, fetch_option = None):
-            try:
-                connection.cursor.execute(query, *args)
-                connection.connection.commit()
-                if fetch_option == "fetchone":
-                    return connection.cursor.fetchone()
-                elif fetch_option == "fetchall":
-                    return connection.cursor.fetchall()
-                else:
-                    return None
-            except Error as e:
-                print(f"Error: {e}")
-                connection.connection.rollback()
+        """
+        Execute an SQL query on the database.
+
+        Parameters:
+        - connection: Database connection object.
+        - query (str): SQL query to be executed.
+        - *args: Query parameters.
+        - fetch_option (str): Option for fetching results ('fetchone' or 'fetchall').
+
+        Returns:
+        - Result of the query depending on the fetch_option.
+        """
+        try:
+            connection.cursor.execute(query, *args)
+            connection.connection.commit()
+            if fetch_option == "fetchone":
+                return connection.cursor.fetchone()
+            elif fetch_option == "fetchall":
+                return connection.cursor.fetchall()
+            else:
+                return None
+        except Error as e:
+            print(f"Error: {e}")
+            connection.connection.rollback()
 
     def create_db_tables(self, connection):
+        """
+        Create database tables if they do not exist.
+
+        Parameters:
+        - connection: Database connection object.
+        """
         self.create_server_periods_table(connection)
         self.create_connections_table(connection)
 
     def create_server_periods_table(self, connection):
+        """
+        Create the server_periods table if it does not exist.
+
+        Parameters:
+        - connection: Database connection object.
+        """
         query = """CREATE TABLE IF NOT EXISTS server_periods(
                     period_id INTEGER PRIMARY KEY,
                     period_start DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -41,6 +89,12 @@ class DatabaseUtils:
         self.execute_sql_query(connection, query)
 
     def create_connections_table(self, connection):
+        """
+        Create the connections table if it does not exist.
+
+        Parameters:
+        - connection: Database connection object.
+        """
         query = """CREATE TABLE IF NOT EXISTS connections(
                    connection_id INTEGER PRIMARY KEY,
                    period_id INTEGER NOT NULL,
@@ -52,30 +106,75 @@ class DatabaseUtils:
         self.execute_sql_query(connection, query)
 
     def add_new_server_period(self, connection):
+        """
+        Adds a new server period to the database.
+
+        Parameters:
+        - connection: Database connection object.
+        """
         query = "INSERT INTO server_periods DEFAULT VALUES"
         self.execute_sql_query(connection, query)
 
     def add_new_connection_to_db(self, connection, airplane_id):
+        """
+        Adds a new connection to the database.
+
+        Parameters:
+        - connection: Database connection object.
+        - airplane_id: ID of the airplane to add to the database.
+        """
         period_id = self.get_period_id(connection)
         query = "INSERT INTO connections (period_id, airplane_id) VALUES (?, ?)"
         self.execute_sql_query(connection, query, (period_id, airplane_id))
 
     def update_connection_status(self, connection, status, airplane_id):
+        """
+        Updates the status of a connection in the database.
+
+        Parameters:
+        - connection: Database connection object.
+        - status: New status value.
+        - airplane_id: ID of the airplane whose connection status is to be updated.
+        """
         period_id = self.get_period_id(connection)
         query = "UPDATE connections SET status = ? WHERE connection_id = (SELECT connection_id FROM connections WHERE period_id = ? AND airplane_id = ?)"
         self.execute_sql_query(connection, query, (status, period_id, airplane_id))
 
     def update_period_end(self, connection):
+        """
+        Updates the end time of the current server period in the database.
+
+        Parameters:
+        - connection: Database connection object.
+        """
         period_id = self.get_period_id(connection)
         query = "UPDATE server_periods SET period_end = CURRENT_TIMESTAMP WHERE period_id = ?"
         self.execute_sql_query(connection, query, (period_id, ))
 
     def get_period_id(self, connection):
+        """
+        Retrieves the ID of the current server period from the database.
+
+        Parameters:
+        - connection: Database connection object.
+
+        Returns:
+        - int: ID of the current server period.
+        """
         period_id_query = "SELECT MAX(period_id) FROM server_periods"
         period_id = self.execute_sql_query(connection, period_id_query, fetch_option = "fetchone")[0]
         return int(period_id)
 
     def get_all_airplanes_number_per_period(self, connection):
+        """
+        Retrieves the number of airplanes for the current period from the database.
+
+        Parameters:
+        - connection: Database connection object.
+
+        Returns:
+        - int: Number of airplanes for the current period.
+        """
         period_id = self.get_period_id(connection)
         query = "SELECT * FROM connections WHERE period_id = ?"
         airplanes_list = self.execute_sql_query(connection, query, (period_id, ), fetch_option = "fetchall")
