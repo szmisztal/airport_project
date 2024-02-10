4c7b8d8bdbff2800949ea4fd88129783c16ea790
szmisztal <sz.misztal@gmail.com>
2024-02-10 11:34:24 +0100

    add doc-strings to all methods in math_patterns.py file

--- math_patterns.py
+++ math_patterns.py
@@ -2,6 +2,20 @@ import math
 
 
 def euclidean_formula(airplane_x, airplane_y, airplane_z, target_x, target_y, target_z):
+    """
+    Calculates the distance between two points in 3D space.
+
+    Parameters:
+    - airplane_x: The x-coordinate of the airplane's current position.
+    - airplane_y: The y-coordinate of the airplane's current position.
+    - airplane_z: The z-coordinate of the airplane's current position.
+    - target_x: The x-coordinate of the target point.
+    - target_y: The y-coordinate of the target point.
+    - target_z: The z-coordinate of the target point.
+
+    Returns:
+    - The distance between the airplane's current position and the target point.
+    """
     distance = math.sqrt((
         pow(airplane_x - target_x, 2) +
         pow(airplane_y - target_y, 2) +
@@ -10,6 +24,18 @@ def euclidean_formula(airplane_x, airplane_y, airplane_z, target_x, target_y, ta
     return round(distance)
 
 def movement_formula(airplane, target_x, target_y, target_z):
+    """
+    Computes the movement of the airplane towards a target point.
+
+    Parameters:
+    - airplane: An instance of the Airplane class representing the airplane.
+    - target_x: The x-coordinate of the target point.
+    - target_y: The y-coordinate of the target point.
+    - target_z: The z-coordinate of the target point.
+
+    Returns:
+    - The updated coordinates (x, y, z) of the airplane after the movement.
+    """
     delta_x = target_x - airplane.x
     delta_y = target_y - airplane.y
     delta_z = target_z - airplane.z
