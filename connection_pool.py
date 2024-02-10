f83540ebff34c68578b3cab16c6af3916a2d25c5
szmisztal <sz.misztal@gmail.com>
2024-02-10 11:25:02 +0100

    add doc-strings to all methods in connection_pool.py file

--- connection_pool.py
+++ connection_pool.py
@@ -7,13 +7,35 @@ from variables import db_file
 
 
 class Connection:
+    """
+    Represents a database connection.
+
+    Attributes:
+    - db_file (str): The path to the SQLite database file.
+    - connection: SQLite connection object.
+    - cursor: Cursor object for executing SQL queries.
+    - in_use (bool): Flag indicating whether the connection is in use.
+    """
+
     def __init__(self):
+        """
+        Initializes a database connection.
+
+        Parameters:
+        - db_file (str): The path to the SQLite database file.
+        """
         self.db_file = db_file
         self.connection = self.create_connection()
         self.cursor = self.connection.cursor()
         self.in_use = False
 
     def create_connection(self):
+        """
+        Creates a connection to the SQLite database.
+
+        Returns:
+        sqlite3.Connection or None: The connection object if successful, None otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file, check_same_thread = False) as connection:
                 return connection
@@ -23,7 +45,26 @@ class Connection:
 
 
 class ConnectionPool:
+    """
+    Manages a pool of database connections.
+
+    Attributes:
+    - connections_list (list): List of available connections.
+    - connections_in_use_list (list): List of connections currently in use.
+    - lock (threading.Lock): Lock for thread-safe access to the connection pool.
+    - min_number_of_connections (int): Minimum number of connections to maintain.
+    - max_number_of_connections (int): Maximum number of connections allowed in the pool.
+    - serialize_utils (SerializeUtils): Utility for serialization.
+    """
+
     def __init__(self, min_numbers_of_connections, max_number_of_connections):
+        """
+        Initializes the connection pool.
+
+        Parameters:
+        - min_number_of_connections (int): Minimum number of connections to maintain.
+        - max_number_of_connections (int): Maximum number of connections allowed in the pool.
+        """
         self.connections_list = []
         self.connections_in_use_list = []
         self.lock = Lock()
@@ -34,11 +75,18 @@ class ConnectionPool:
         self.connections_manager()
 
     def create_start_connections(self):
+        """Creates the initial connections in the pool."""
         for _ in range(self.min_number_of_connections):
             connection = Connection()
             self.connections_list.append(connection)
 
     def get_connection(self):
+        """
+        Retrieves a connection from the pool.
+
+        Returns:
+        - Connection: A connection object if available, False otherwise.
+        """
         with self.lock:
             for connection in self.connections_list:
                 if connection.in_use == False:
@@ -56,6 +104,12 @@ class ConnectionPool:
                 return False
 
     def release_connection(self, connection):
+        """
+        Releases a connection back to the pool.
+
+        Parameters:
+        - connection (Connection): The connection to release.
+        """
         with self.lock:
             if connection in self.connections_in_use_list:
                 connection.in_use = False
@@ -63,6 +117,7 @@ class ConnectionPool:
                 self.connections_in_use_list.remove(connection)
 
     def destroy_unused_connections(self):
+        """Destroys unused connections in excess of the minimum number."""
         with self.lock:
             for connection in self.connections_list:
                 if len(self.connections_list) > 11:
@@ -72,6 +127,7 @@ class ConnectionPool:
                         break
 
     def keep_connections_at_the_starting_level(self):
+        """Ensures the connection pool maintains the minimum number of connections."""
         if len(self.connections_list) < self.min_number_of_connections:
             for _ in self.connections_list:
                 connection = Connection()
@@ -80,5 +136,6 @@ class ConnectionPool:
                     break
 
     def connections_manager(self):
+        """Manages connection pool tasks."""
         schedule.every(1).minute.do(self.keep_connections_at_the_starting_level)
         schedule.every(1).minute.do(self.destroy_unused_connections)
