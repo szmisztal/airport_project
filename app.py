9e7c1257c0d3a8e3f917404371eae93a40cf46a4
szmisztal <sz.misztal@gmail.com>
2024-02-25 14:38:58 +0100

    test basic flask url

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from client_side.client import Client
+from server_side.server import Server
+from server_side.connection_pool import ConnectionPool
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def airport_start():
-    c = Client()
-    c.start()
+    c = ConnectionPool(10, 100)
+    s = Server(c)
+    s.start()
     return "."
 
 if __name__ == "__main__":
