4e965b09f2127270aa1dccbd69d430bda67c9b06
szmisztal <sz.misztal@gmail.com>
2024-02-27 11:50:11 +0100

    add all neded routs to code

--- app.py
+++ app.py
@@ -6,12 +6,46 @@ from server_side.connection_pool import ConnectionPool
 
 app = Flask(__name__)
 
-@app.route("/")
-def airport_start():
-    c = ConnectionPool(10, 100)
-    s = Server(c)
-    s.start()
-    return "."
+@app.route("/start_airport")
+def start_airport():
+    pass
+
+@app.route("/pause_airport")
+def pause_airport():
+    pass
+
+@app.route("/restore_airport")
+def restore_airport():
+    pass
+
+@app.route("/close_airport")
+def close_airport():
+    pass
+
+@app.route("/uptime")
+def uptime():
+    pass
+
+@app.route("/count_airplanes")
+def count_airplanes():
+    pass
+
+@app.route("/collisions")
+def collisions():
+    pass
+
+@app.route("/successfully_landings")
+def successfully_landings():
+    pass
+
+@app.route("/airplanes_in_the_air")
+def airplanes_in_the_air():
+    pass
+
+@app.route("/airplanes/<int:airplane_id>")
+def airplane_detail(id):
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug = True)
