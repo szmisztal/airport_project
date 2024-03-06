c63f27153bc3c23f6d4a182f7990c13d60efb532
szmisztal <sz.misztal@gmail.com>
2024-03-06 14:43:47 +0100

    try to close server by request, but I dont know how to do it yet

--- app.py
+++ app.py
@@ -4,20 +4,32 @@ from flask import Flask, jsonify
 from config_variables_for_server_and_client import logger_config
 
 
-app = Flask(__name__)
 logger = logger_config("API logger", os.getcwd(), "api_logs.log")
+app = Flask(__name__)
 process = None
-server_process_pid = {}
 
 
 @app.route("/start")
 def start_airport():
     global process
     server_script_path = f"{os.getcwd()}/server_side/server.py"
-    process = subprocess.Popen(["python", server_script_path], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+    process = subprocess.Popen(["python", server_script_path, "start"], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
     logger.info(f"Started script with PID {process.pid}")
-    server_process_pid["server_process_pid"] = process.pid
     return jsonify({"message": "Server started", "pid": process.pid})
+
+@app.route("/close")
+def close_airport():
+    global process
+    if process:
+        process.terminate()
+        process.wait()
+        response = {"message": "Server stopped", "pid": process.pid}
+        logger.info(f"Close script with PID {process.pid}")
+        process = None
+        return jsonify(response)
+    else:
+        logger.info("Server is not running")
+        return jsonify({"error": "server is not running"})
 
 @app.route("/pause")
 def pause_airport():
@@ -26,19 +38,6 @@ def pause_airport():
 @app.route("/restore")
 def restore_airport():
     pass
-
-@app.route("/close")
-def close_airport():
-    global process
-    process_pid = server_process_pid.get("server_process_pid")
-    if process:
-        process.terminate()
-        process.wait()
-        logger.info(f"Close script with PID {process_pid}")
-        return jsonify({"message": "Server`s close", "pid": process_pid})
-    else:
-        logger.info("Server is not running")
-        return jsonify({"error": "server is not running"})
 
 @app.route("/uptime")
 def uptime():
