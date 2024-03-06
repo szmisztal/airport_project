12fdead076fc1afe1aa2980d8d6cec92378c359d
szmisztal <sz.misztal@gmail.com>
2024-03-06 12:38:56 +0100

    add subprocess lib, and write first API endpoint to start server by request

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import os
+import subprocess
+from flask import Flask, jsonify
+from config_variables_for_server_and_client import logger_config
 
 
 app = Flask(__name__)
+logger = logger_config("API logger", os.getcwd(), "api_logs.log")
 
 
 @app.route("/start")
 def start_airport():
-    pass
+    server_script_path = f"{os.getcwd()}/server_side/server.py"
+    process = subprocess.Popen(["python", server_script_path], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+    stdout, stderr = process.communicate()
+    logger.info(f"{process.returncode} - stdout: {stdout}, stderr: {stderr}")
+    return jsonify({
+        "stdout": stdout,
+        "stderr": stderr,
+        "returncode": process.returncode
+    })
 
 @app.route("/pause")
 def pause_airport():
