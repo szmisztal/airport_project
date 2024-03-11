f6d6bfcbe46dcf3e21f7c858652dacb8e7f929f2
szmisztal <sz.misztal@gmail.com>
2024-03-11 15:57:45 +0100

    add REST methods to routes and HTTP status codes

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ class API:
         connection (Connection): Connection object to interact with the database.
         db_utils (DatabaseUtils): Utility object for database operations.
         process (subprocess.Popen): Represents the running server process.
-        response_when_server_is_not_running (dict): Default response when the server is not running.
     """
 
     def __init__(self):
@@ -33,7 +32,6 @@ class API:
         self.connection = Connection(db_file)
         self.db_utils = DatabaseUtils()
         self.process = None
-        self.response_when_server_is_not_running = {"error": "server is not running"}
 
     def server_start(self):
         """
@@ -53,83 +51,65 @@ class API:
         Terminates the server subprocess, updates the database, and closes the database connection.
 
         Returns:
-            dict: A response message indicating the server has stopped or an error message if the server wasn't running.
+            dict: A response message indicating the server has stopped.
         """
-        if self.is_running:
-            self.process.terminate()
-            self.process.wait()
-            response = {"message": "Server stopped", "pid": self.process.pid}
-            self.logger.info(f"Close script with PID {self.process.pid}")
-            self.process = None
-            self.db_utils.update_period_end(self.connection)
-            self.connection.connection.close()
-            return response
-        else:
-            return self.response_when_server_is_not_running
+        self.process.terminate()
+        self.process.wait()
+        self.logger.info(f"Close script with PID {self.process.pid}")
+        self.process = None
+        self.db_utils.update_period_end(self.connection)
+        self.connection.connection.close()
+        return {"message": "Server stopped", "pid": self.process.pid}
 
     def server_pause(self):
         """
         Creates a flag file to signal the server to pause its operations.
 
         Returns:
-            dict: A response indicating the server's paused status or an error message if the server wasn't running.
+            dict: A response indicating the server's paused status.
         """
-        if self.is_running:
-            path = f"{os.getcwd()}\\server_side\\flag_file.txt"
-            with open(path, "w"):
-                pass
-            response = {"server status": "paused"}
-            return response
-        else:
-            return self.response_when_server_is_not_running
+        path = f"{os.getcwd()}\\server_side\\flag_file.txt"
+        with open(path, "w"):
+            pass
+        return {"server status": "paused"}
 
     def server_resume(self):
         """
         Removes the flag file to signal the server to resume its operations.
 
         Returns:
-            dict: A response indicating the server's resumed status or an error message if the server wasn't running or wasn't paused.
+            dict: A response indicating the server's resumed status or None if file not exists.
         """
-        if self.is_running:
-            path = f"{os.getcwd()}\\server_side\\flag_file.txt"
-            file = os.path.isfile(path)
-            if file:
-                os.remove(path)
-                response = {"server status": "resumed"}
-                return response
-            else:
-                return self.response_when_server_is_not_running
+        path = f"{os.getcwd()}\\server_side\\flag_file.txt"
+        file = os.path.isfile(path)
+        if file:
+            os.remove(path)
+            return {"server status": "resumed"}
         else:
-            return self.response_when_server_is_not_running
+            return None
 
     def server_uptime(self):
         """
         Calculates and returns the server's uptime by comparing the current time with the last recorded start time.
 
         Returns:
-            str: The server uptime duration or an error message if the server isn't running.
+            str: The server uptime duration.
         """
-        if self.is_running:
-            date_format = "%Y-%m-%d %H:%M:%S"
-            current_time = dt.datetime.now()
-            server_start_time = self.db_utils.get_last_period_start_date(self.connection)
-            server_uptime = current_time - dt.datetime.strptime(server_start_time, date_format)
-            return str(server_uptime)
-        else:
-            return self.response_when_server_is_not_running
+        date_format = "%Y-%m-%d %H:%M:%S"
+        current_time = dt.datetime.now()
+        server_start_time = self.db_utils.get_last_period_start_date(self.connection)
+        server_uptime = current_time - dt.datetime.strptime(server_start_time, date_format)
+        return str(server_uptime)
 
     def number_of_airplanes(self):
         """
         Retrieves the total number of airplanes being tracked for the current period.
 
         Returns:
-            dict or int: The number of airplanes or an error message if the server isn't running.
+            int: The number of airplanes.
         """
-        if self.is_running:
-            all_airplanes = self.db_utils.get_all_airplanes_number_per_period(self.connection)
-            return all_airplanes
-        else:
-            return self.response_when_server_is_not_running
+        all_airplanes = self.db_utils.get_all_airplanes_number_per_period(self.connection)
+        return all_airplanes
 
     def airplanes_by_status(self, status):
         """
@@ -139,13 +119,10 @@ class API:
             status (str): The status to filter the airplanes by.
 
         Returns:
-            dict or list: A list of airplanes with the specified status or an error message if the server isn't running.
+            dict or list: A list of airplanes with the specified status.
         """
-        if self.is_running:
-            airplanes_with_specified_status = self.db_utils.get_airplanes_with_specified_status_per_period(self.connection, status)
-            return airplanes_with_specified_status
-        else:
-            return self.response_when_server_is_not_running
+        airplanes_with_specified_status = self.db_utils.get_airplanes_with_specified_status_per_period(self.connection, status)
+        return airplanes_with_specified_status
 
     def single_airplane_details(self, id):
         """
@@ -155,17 +132,14 @@ class API:
             id (int): The ID of the airplane to retrieve details for.
 
         Returns:
-            dict: The details of the specified airplane or an error message if the server isn't running.
+            dict: The details of the specified airplane.
         """
-        if self.is_running:
-            airplane_id = f"Airplane_{id}"
-            airplane_details = self.db_utils.get_single_airplane_details(self.connection, airplane_id)
-            return airplane_details
-        else:
-            return self.response_when_server_is_not_running
+        airplane_id = f"Airplane_{id}"
+        airplane_details = self.db_utils.get_single_airplane_details(self.connection, airplane_id)
+        return airplane_details
 
 
-@app.route("/start")
+@app.route("/start", methods = ["GET"])
 def start_airport():
     """
     Starts the airport server process.
@@ -174,9 +148,9 @@ def start_airport():
         JSON response indicating that the server has started and its PID.
     """
     process = api.server_start()
-    return jsonify({"message": "Server started", "pid": process.pid})
+    return jsonify({"message": "Server started", "pid": process.pid}), 200
 
-@app.route("/close")
+@app.route("/close", methods = ["GET"])
 def close_airport():
     """
     Closes the airport server process.
@@ -184,10 +158,13 @@ def close_airport():
     Returns:
         JSON response indicating that the server has stopped.
     """
-    response = api.server_close()
-    return jsonify(response)
+    if api.is_running:
+        response = api.server_close()
+        return jsonify(response), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/pause")
+@app.route("/pause", methods = ["GET"])
 def pause_airport():
     """
     Pauses the airport server operations.
@@ -195,10 +172,13 @@ def pause_airport():
     Returns:
         JSON response indicating that the server is paused.
     """
-    pause_server = api.server_pause()
-    return jsonify(pause_server)
+    if api.is_running:
+        response = api.server_pause()
+        return jsonify(response), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/restore")
+@app.route("/restore", methods = ["GET"])
 def restore_airport():
     """
     Resumes the airport server operations.
@@ -206,10 +186,16 @@ def restore_airport():
     Returns:
         JSON response indicating that the server operations have been resumed.
     """
-    resume_server = api.server_resume()
-    return jsonify(resume_server)
+    if api.is_running:
+        resume_server = api.server_resume()
+        if resume_server is None:
+            return jsonify({"error": "server is working right now"}), 400
+        else:
+            return jsonify(resume_server), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/uptime")
+@app.route("/uptime", methods = ["GET"])
 def uptime():
     """
     Provides the uptime of the airport server.
@@ -217,10 +203,13 @@ def uptime():
     Returns:
         JSON response with the server uptime duration.
     """
-    server_uptime = api.server_uptime()
-    return jsonify({"server uptime": server_uptime})
+    if api.is_running:
+        server_uptime = api.server_uptime()
+        return jsonify({"server uptime": server_uptime}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/airplanes")
+@app.route("/airplanes", methods = ["GET"])
 def airplanes():
     """
     Retrieves the total number of airplanes.
@@ -228,10 +217,13 @@ def airplanes():
     Returns:
         JSON response with the total number of airplanes.
     """
-    airplanes = api.number_of_airplanes()
-    return jsonify({"all airplanes number": airplanes})
+    if api.is_running:
+        airplanes = api.number_of_airplanes()
+        return jsonify({"all airplanes number": airplanes}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/collisions")
+@app.route("/collisions", methods = ["GET"])
 def collisions():
     """
     Retrieves the list of airplanes that crashed due to running out of fuel and due to collision.
@@ -239,23 +231,29 @@ def collisions():
     Returns:
         JSON response with the list of airplanes crashed by running out of fuel and by collision.
     """
-    airplanes_crashed_by_out_of_fuel = api.airplanes_by_status("CRASHED BY OUT OF FUEL")
-    airplanes_crashed_by_collision = api.airplanes_by_status("CRASHED BY COLLISION")
-    return jsonify({"airplanes crashed by out of fuel": airplanes_crashed_by_out_of_fuel,
-                    "airplanes crashed by collision": airplanes_crashed_by_collision})
+    if api.is_running:
+        airplanes_crashed_by_out_of_fuel = api.airplanes_by_status("CRASHED BY OUT OF FUEL")
+        airplanes_crashed_by_collision = api.airplanes_by_status("CRASHED BY COLLISION")
+        return jsonify({"airplanes crashed by out of fuel": airplanes_crashed_by_out_of_fuel,
+                        "airplanes crashed by collision": airplanes_crashed_by_collision}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/landings")
-def successfully_landings():
+@app.route("/landings", methods = ["GET"])
+def successful_landings():
     """
     Retrieves the list of airplanes that landed successfully.
 
     Returns:
         JSON response with the list of airplanes that had a successful landing.
     """
-    airplanes_with_successfully_landings = api.airplanes_by_status("SUCCESSFULLY LANDING")
-    return jsonify({"airplanes with successfully landing": airplanes_with_successfully_landings})
+    if api.is_running:
+        airplanes_with_successfully_landings = api.airplanes_by_status("SUCCESSFULLY LANDING")
+        return jsonify({"airplanes with successfully landing": airplanes_with_successfully_landings}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/airplanes_in_the_air")
+@app.route("/airplanes_in_the_air", methods = ["GET"])
 def airplanes_in_the_air():
     """
     Retrieves the list of airplanes that are currently in the air.
@@ -263,10 +261,13 @@ def airplanes_in_the_air():
     Returns:
         JSON response with the list of airplanes that are in the air.
     """
-    planes_in_the_air = api.airplanes_by_status(None)
-    return jsonify({"airplanes in the air": planes_in_the_air})
+    if api.is_running:
+        planes_in_the_air = api.airplanes_by_status(None)
+        return jsonify({"airplanes in the air": planes_in_the_air}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
-@app.route("/airplanes/<int:airplane_id>")
+@app.route("/airplanes/<int:airplane_id>", methods = ["GET"])
 def airplane_detail(airplane_id):
     """
     Retrieves details of a specific airplane by its ID.
@@ -277,19 +278,21 @@ def airplane_detail(airplane_id):
     Returns:
         JSON response with the details of the specified airplane or an error message if the airplane ID does not exist.
     """
-    airplane = api.single_airplane_details(airplane_id)
-    if airplane == None:
-        return jsonify({"error": "there is no airplane with this id"})
-    id = airplane[0]
-    airplane_appearance_date = airplane[1]
-    status = airplane[2]
-    if status == None:
-        status = "In the air"
-    return jsonify({"airplane details":
-                        {"id": id,
-                         "appearance_date": airplane_appearance_date,
-                         "status": status}
-                    })
+    if api.is_running:
+        airplane = api.single_airplane_details(airplane_id)
+        if airplane == None:
+            return jsonify({"error": "there is no airplane with this id"}), 404
+        id = airplane[0]
+        airplane_appearance_date = airplane[1]
+        status = airplane[2]
+        if status == None:
+            status = "In the air"
+        return jsonify({"airplane details":
+                            {"id": id,
+                             "appearance_date": airplane_appearance_date,
+                             "status": status}}), 200
+    else:
+        return jsonify({"error": "server is not running"}), 400
 
 
 if __name__ == "__main__":
